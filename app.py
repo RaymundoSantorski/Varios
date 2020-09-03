c70b0c46176154454d33b6e45f24891481f4efbf
RaymundoSantorski <rayma9829@gmail.com>
2020-09-03 13:41:54 -0500

    Nuevas funciones en python

--- app.py
+++ app.py
@@ -4,6 +4,20 @@ def potencia(base, potencia):
         resultado=resultado*base
     return resultado
 
-base = int(input("Base "))
-pot = int(input("Potencia "))
-print("El resultado es: ",potencia(base, pot))
+def factorial(n):
+    if n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+def elec():
+    opc = input("¿Factorial o Potencia?")
+    if opc == "Factorial":
+        fac = int(input("Número para calcular el factorial "))
+        print(factorial(fac))
+    elif opc == "Potencia":
+        base = int(input("Base "))
+        pot = int(input("Potencia "))
+        print(potencia(base, pot))
+
+elec()
