911d67f67ce8a6ceb15bbc8952ade6fbe590b4ee
RaymundoSantorski <rayma9829@gmail.com>
2020-09-04 01:05:39 -0500

    Nueva funcion Python

--- app.py
+++ app.py
@@ -10,8 +10,25 @@ def factorial(n):
     else:
         return n*factorial(n-1)
 
+def primo(n):
+    divisores=0
+    for i in range(int(n/2)):
+        if n%(i+1)==0:
+            divisores+=1
+    return divisores==1
+
+def perfectos(n):
+    for i in range(n):
+        if primo(i) == True:
+            if primo(potencia(2,i)-1) == True:
+                perfecto = potencia(2,(i-1))*(potencia(2,i)-1)
+                print(perfecto)
+
+
+
+
 def elec():
-    opc = input("¿Factorial o Potencia?")
+    opc = input("¿Factorial, Potencia, Primo o Perfecto?")
     if opc == "Factorial":
         fac = int(input("Número para calcular el factorial "))
         print(factorial(fac))
@@ -19,5 +36,12 @@ def elec():
         base = int(input("Base "))
         pot = int(input("Potencia "))
         print(potencia(base, pot))
+    elif opc == "Primo":
+        n = int(input("Numero a evaluar"))
+        print(primo(n))
+    elif opc == "Perfecto":
+        n = int(input("¿Cuantos numeros perfectos?"))
+        perfectos(n)
+
 
 elec()
