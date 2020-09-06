bb5c6424ec766cb23435d322de7df09254f9cdde
RaymundoSantorski <rayma9829@gmail.com>
2020-09-05 22:29:05 -0500

    Tratando de agregar items a una lista

--- static/main.js
+++ static/main.js
@@ -9,3 +9,8 @@ function remover(){
     var elemento = document.getElementById("overlay");
     elemento.classList.remove('active');
 }
+
+function add{
+    var contenido = document.getElementById("items").innerHTML;
+    
+}
