e107ab2a033fc49ba1cd201497947e57785cd4df
RaymundoSantorski <rayma9829@gmail.com>
2020-09-09 00:49:26 -0500

    commit

--- app2.py
+++ app2.py
@@ -13,5 +13,10 @@ def product():
 def carrito():
     return render_template("carrito.html")
 
+@app.route('/successful')
+def successful():
+    return render_template("successful.html")
+
+
 if __name__ == '__main__': 
     app.run(debug=True)
