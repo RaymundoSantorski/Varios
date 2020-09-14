88f8243c53ad4a07cb9756c0b4adfb06d1fa7925
RaymundoSantorski <rayma9829@gmail.com>
2020-09-14 15:04:53 -0500

    borrar imagenes del servidor

--- app2.py
+++ app2.py
@@ -28,8 +28,7 @@ def product():
 def carrito():
     if not os.path.isdir(target):
         os.mkdir(target)
-    else:
-        print("couldnt create upload directory: {}".format(target))
+
     if request.method == 'POST':
         producto = request.form['producto']
         imagen = request.files['imagen']
@@ -58,11 +57,58 @@ def successful():
 def delete(id):
     con = sqlite3.connect('mydb.db')
     cur = con.cursor()
+    cur.execute('SELECT Imagen FROM Productos WHERE ID = ?', (id,))
+    img = cur.fetchone()
     cur.execute('DELETE FROM Productos WHERE ID = ?', (id,))
     con.commit()
-    target.os.remove()
+    print(img[0])
+    os.remove('static/'+img[0])
     flash('Producto eliminado satisfactoriamente')
     return redirect(url_for('index'))
+
+@app.route("/edit/<id>")
+def edit(id):
+    con = sqlite3.connect('mydb.db')
+    cur = con.cursor()
+    cur.execute('SELECT * FROM Productos WHERE ID = ?', (id,))
+    data = cur.fetchone()
+    return render_template("edit.html", producto = data)
+
+@app.route("/update/<id>", methods = ['POST'])
+def update(id):
+    if request.method == 'POST':
+        producto = request.form['producto']
+        imagen = request.files['imagen']
+        descripcion = request.form['descripcion']
+        precio = request.form['precio']
+        filename = imagen.filename
+        destination = "/".join([target, filename])
+        con = sqlite3.connect('mydb.db')
+        cur = con.cursor()
+        if imagen:
+            cur.execute('SELECT Imagen FROM Productos WHERE ID = ?', (id,)) 
+            ind = cur.fetchone()
+            img = ind[0]
+            os.remove('static/'+img)
+            cur.execute("""
+                UPDATE Productos
+                SET Imagen = ?
+                WHERE ID = ?
+            """, (filename, id))
+            con.commit()
+            imagen.save(destination)
+        if producto:
+            cur.execute("UPDATE Productos SET Producto = ? WHERE ID = ?", (producto, id))
+            con.commit()
+        if descripcion:
+            cur.execute("UPDATE Productos SET descripcion = ? WHERE ID = ?", (descripcion, id))
+            con.commit()
+        if precio:
+            precioFormat = int(precio)
+            cur.execute("UPDATE Productos SET Precio = ? WHERE ID = ?", (precioFormat, id))
+            con.commit()
+        flash('Producto acualizado satisfactoriamente')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__': 
     app.run(debug=True)
