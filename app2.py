2d6d25f119ded74ba27d277d98b2a52315f1a1ac
RaymundoSantorski <rayma9829@gmail.com>
2020-09-14 17:45:36 -0500

    commit

--- app2.py
+++ app2.py
@@ -1,34 +1,53 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3, os
 
-
 app = Flask(__name__)
 
 #conección a sqlite3
 
 # settings
 app.secret_key = 'mysecretkey'
-
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 target = os.path.join(APP_ROOT, 'static/')
 
-@app.route("/") 
-def index(): 
-    con = sqlite3.connect('mydb.db')
-    cur = con.cursor()
-    cur.execute('SELECT * FROM Productos')
-    data = cur.fetchall()
-    return render_template("index.html", productos = data)
+@app.route("/storeManager", methods =['POST','GET']) 
+def storeManager():
+    if request.method == 'POST':
+        user = request.form['usuario']
+        password = request.form['contrasena']
+        con = sqlite3.connect('mydb.db')
+        cur = con.cursor()
+        cur.execute('SELECT * FROM Usuarios')
+        usuarios = cur.fetchall()
+        auten = False
+        for usuario in usuarios:
+            if usuario[1]==user and usuario[0]==password:
+                auten = True
+                break
+        
+        if auten == True:   
+            cur.execute('SELECT * FROM Productos')
+            data = cur.fetchall()
+            access = True
+            flash('Bienvenido')
+            return render_template("storeManager.html", productos = data)
+        else:
+            flash('El usuario o la contraseña son incorrectos')
+            return render_template("autenticar.html")
+    else:
+        if access == True:
+            return render_template("storeManager.html")
+        else:
+            return render_template("autenticar.html")
 
 @app.route("/productos")
 def product():
     return render_template("producto.html")
 
-@app.route("/carrito", methods = ['POST'])
-def carrito():
+@app.route("/add", methods = ['POST'])
+def add():
     if not os.path.isdir(target):
         os.mkdir(target)
-
     if request.method == 'POST':
         producto = request.form['producto']
         imagen = request.files['imagen']
@@ -48,7 +67,7 @@ def carrito():
             cur.execute('INSERT INTO Productos (Producto, Imagen, Descripcion, Precio) VALUES (?,?,?,?)',(producto, filename, descripcion, precio))
             con.commit()
             flash('Producto agregado satisfactoriamente')
-            return redirect(url_for('index'))
+            return redirect(url_for('storeManager'))
 
 
 @app.route("/successful")
@@ -66,7 +85,7 @@ def delete(id):
     print(img[0])
     os.remove('static/'+img[0])
     flash('Producto eliminado satisfactoriamente')
-    return redirect(url_for('index'))
+    return redirect(url_for('storeManager'))
 
 @app.route("/edit/<id>")
 def edit(id):
@@ -109,8 +128,8 @@ def update(id):
             precioFormat = int(precio)
             cur.execute("UPDATE Productos SET Precio = ? WHERE ID = ?", (precioFormat   , id))
             con.commit()
-        flash('Producto acualizado satisfactoriamente')
-        return redirect(url_for('index'))
+        flash('Producto actualizado satisfactoriamente')
+        return redirect(url_for('storeManager'))
 
 if __name__ == '__main__': 
     app.run(debug=True)
